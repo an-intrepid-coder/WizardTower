f8bdf9339174a9666c9f2acc534f6de797378fa8
Samuel Gibson <sgibber2018@gmail.com>
2022-07-06 20:57:19 -0700

    InventoryItem.kt -- Implemented more aspects of Inventory Items. Implemented some more individual items.

--- src/main/kotlin/game/InventoryItem.kt
+++ src/main/kotlin/game/InventoryItem.kt
@@ -13,17 +13,19 @@ enum class DamageType { // TODO: Implement various damage types in combat system
 /**
  * Items which can be armor, weapons, or anything else.
  */
-class InventoryItem(
+sealed class InventoryItem(
     val name: String,
+    val weight: Int,
+    val cost: Int,
     val damageResistance: Int,
     val armor: Boolean,
-    // TODO: Weapon stuff. Most attacks will happen via the weapon's ability/effect through the menu. <-- BOOKMARK
+    val shield: Boolean,
+    val defenseBonus: Int,
+    val weapon: Boolean,
     val twoHanded: Boolean,
-    val useEffect: ((WizardTowerGame, Actor, Actor?) -> Unit)?,  // game, user, target (if any)
-    // TODO: Charges and other specifics of useEffect()
+    val useEffect: ((WizardTowerGame, Character, Character?) -> Unit)?,  // game, user, target (if any)
 ) {
     var equipped: Boolean = false
-
 
     /**
      * Describes the InventoryItem with a list of Messages.
@@ -37,4 +39,61 @@ class InventoryItem(
     override fun toString(): String {
         return name
     }
+
+    // NOTE: Each individual class of item will be extensible with proc-gen, so that they are more like
+    //       template classes. TODO: Implement procedural versions.
+
+    class PaddedArmor : InventoryItem(
+        name = "Padded Armor",
+        weight = 12,
+        cost = 150,
+        damageResistance = 1,
+        armor = true,
+        shield = false,
+        defenseBonus = 0,
+        weapon = false,
+        twoHanded = false,
+        useEffect = null,
+    )
+
+    class SmallShield : InventoryItem(
+        name = "Small Shield",
+        weight = 8,
+        cost = 40,
+        damageResistance = 0,
+        armor = false,
+        shield = true,
+        defenseBonus = 1,
+        weapon = false,
+        twoHanded = false,
+        useEffect = null,
+    )
+
+    class MediumShield : InventoryItem(
+        name = "Medium Shield",
+        weight = 15,
+        cost = 60,
+        damageResistance = 0,
+        armor = false,
+        shield = true,
+        defenseBonus = 2,
+        weapon = false,
+        twoHanded = false,
+        useEffect = null,
+    )
+
+    class LargeShield : InventoryItem(
+        name = "Large Shield",
+        weight = 25,
+        cost = 90,
+        damageResistance = 0,
+        armor = false,
+        shield = true,
+        defenseBonus = 3,
+        weapon = false,
+        twoHanded = false,
+        useEffect = null,
+    )
+
+    // TODO: More individual items as their own classes, with room for some proc-gen.
 }
